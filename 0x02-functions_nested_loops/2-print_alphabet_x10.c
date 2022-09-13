29759ff5c5a7284ff893b7bd22f71efe980f3cff
iam-phenomenal <ayandiranibrahim@outlook.com>
2022-09-13 20:46:32 +0100

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,9 +12,12 @@ void print_alphabet_x10(void)
 
 	while (count <= 9)
 	{
-		for (char c = 'a'; c <= 'z'; c++)
+		char c = 'a';
+
+		while (c <= 'z')
 		{
 			_putchar(c);
+			c++;
 		}
 		_putchar('\n');
 		count++;
