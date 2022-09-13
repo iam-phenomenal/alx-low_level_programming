48925ebf913ba62a211c766dbe0199ae186686dc
iam-phenomenal <ayandiranibrahim@outlook.com>
2022-09-13 20:36:30 +0100

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * print_prototype_x10- Entry
+ * print_alphabet_x10- Entry
  * Description: Printing lowercase alphabet 10 times
  * Return: 0(Success)
 */
+
 void print_alphabet_x10(void)
 {
-	int count= 0;
+	int count = 0;
 
 	while (count <= 9)
 	{
-		for (char c='a'; c <='z'; c++)
+		for (char c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
