79827e8b99776e73bd1563e1af8f96120a1513d0
iam-phenomenal <ayandiranibrahim@outlook.com>
2022-09-13 19:31:06 +0100

    Task 1 lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main- Entry
+ * Description: Print lowercase alphabets
+ * Return: 0(Success)
+*/
+
+int main(void)
+{
+	char start = 'a';
+	for (start; start <= z; start++)
+	{
+		_putchar(start);
+	}
+	_putchar('\n');
+	return (0);
+}
