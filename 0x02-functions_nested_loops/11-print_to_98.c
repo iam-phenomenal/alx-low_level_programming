555e74a0064a13b714664d433a2390ca6567d81f
iam-phenomenal <ayandiranibrahim@outlook.com>
2022-09-14 06:04:46 +0100

    Task 11 Check

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 /**
  * print_to_98- Entry
  * @n: Integer starting point
@@ -8,20 +8,20 @@
 */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n < 98)
 	{
-		while (n <= 98)
+		while (n < 98)
 		{
-			printf("%d", n);
+			printf("%d, ", n);
 			n++;
 		}
 	}
-	else if (n >= 98)
+	else if (n > 98)
 	{
-		while (n >= 98)
+		while (n > 98)
 		{
-			printf("%d", n);
+			printf("%d, ", n);
 			n--;
 		}
 	}
-	return;
+	printf("98\n");
