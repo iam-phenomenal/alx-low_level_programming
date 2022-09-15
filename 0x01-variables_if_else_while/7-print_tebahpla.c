3c61cb5d53182b4c4aca3a3132c7b045dd31000f
iam-phenomenal <ayandiranibrahim@outlook.com>
2022-09-15 16:39:01 +0100

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- Entry
+ * Description: Print alphabets in reverse
+ * Return: 0(Success)
+*/
+int main(void)
+{
+	char start_letter = 'z';
+
+	while (start_letter >= 'a')
+	{
+		putchar(start_letter);
+		start_letter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
