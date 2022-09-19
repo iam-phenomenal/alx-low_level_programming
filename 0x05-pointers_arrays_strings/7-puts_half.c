a35ab61d21fd07235f9c2c780c782705a3b64dfa
iam-phenomenal <ayandiranibrahim@outlook.com>
2022-09-19 21:49:53 +0100

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half- Entry
+ * @str: string to be manipulated
+ * Description: Prints half of string
+ * Return: void
+*/
+void puts_half(char *str)
+{
+	int len = _strlen(str);
+
+	int n = (len - 1) / 2;
+
+	while (str[n] != '\0')
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
+}
