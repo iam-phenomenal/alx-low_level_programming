44aaa1da24acd5caab2875922ab08135e252a87d
iam-phenomenal <ayandiranibrahim@outlook.com>
2022-09-19 14:59:45 +0100

    Task 1 Swapping values

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,4 +1,15 @@
 #include "main.h"
 
 /**
- *
+ * swap_int- Entry
+ * @a: First swap value
+ * @b: Second swap value
+ * Description: Swap the the value of a and b
+ * Return:
+*/
+
+void swap_int(int *a, int *b)
+{
+	*a = &b;
+	*b = &a;
+}
