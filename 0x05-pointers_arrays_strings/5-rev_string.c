ef12d124f3325f2985b4502b4c401e2a374f687b
iam-phenomenal <ayandiranibrahim@outlook.com>
2022-09-19 18:17:37 +0100

    Task 5 Print String in Reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev- Entry
+ * @s: String to be printed
+ * Description: Print string in reverse
+ * Return: void
+*/
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	while (length > 0)
+	{
+		_putchar(s[length - 1]);
+		length--;
+	}
+
+}
