c55bc8c297d114a65a48ff38d0f29d54f566edb7
iam-phenomenal <ayandiranibrahim@outlook.com>
2022-09-19 17:06:19 +0100

    Task 2 String counter

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen- Entry
+ * @s: String to be counter
+ * Description: Counts the number of characters in a string
+ * Return: Length of string
+*/
+
+int _strlen(char *s)
+{
+	int index = 0;
+
+	while (s[index] != '\0')
+	{
+		index++;
+	}
+	return (index + 1);
+}
