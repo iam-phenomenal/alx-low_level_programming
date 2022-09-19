2e6b85503e1986fa9e4bf7d923ed7d4edf76a506
iam-phenomenal <ayandiranibrahim@outlook.com>
2022-09-19 14:45:10 +0100

    Pointers, Arrays and String initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98- Entry
+ * @n: Integer value pointer
+ * Description: Reset pointer value to 98
+ * Return: Void
+*/
+
+void reset_to_98(int *n)
+{
+	int new_value = 98;
+	*n = &new_value;
+}
