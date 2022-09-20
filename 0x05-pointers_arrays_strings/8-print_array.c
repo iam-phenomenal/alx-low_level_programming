84e0024e957dd3493bbe6c35d8647e114c36ace4
iam-phenomenal <ayandiranibrahim@outlook.com>
2022-09-20 05:20:30 +0100

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_array- Entry
+ * @a: array pointer
+ * @n: number of arrays to be printed
+ * Description: prints specified number of array
+ * Return: void
+*/
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	n--;
+
+	while (n >= 0)
+	{
+		printf("%d", a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+		n--;
+		i++;
+	}
+	printf("\n");
+}
