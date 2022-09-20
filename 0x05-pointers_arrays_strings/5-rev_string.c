d52139e7a9ed83d650a2660b3a8a49be97972441
iam-phenomenal <ayandiranibrahim@outlook.com>
2022-09-20 04:51:06 +0100

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,16 +9,19 @@
 void rev_string(char *s)
 {
 	int length = 0;
+	int rev_index = 0;
+	char tmp_holder = *s
 
 	while (s[length] != '\0')
 	{
 		length++;
 	}
+	length--;
 
-	while (length > 0)
+	while(length >= 0)
 	{
-		_putchar(s[length - 1]);
+		s[rev_index] = tmp_holder[length];
+		rev_index++;
 		length--;
 	}
-
 }
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,7 +16,14 @@ void puts_half(char *str)
 		len++;
 	}
 
-	n = (len) / 2;
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (len - 1) / 2;
+	}
 
 	while (str[n] != '\0')
 	{
