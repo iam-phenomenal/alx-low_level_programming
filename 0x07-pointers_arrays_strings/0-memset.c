3ec45d0a2a5d48a281453e1c22be14fd87b91f2e
iam-phenomenal <ayandiranibrahim@outlook.com>
2022-09-27 01:40:23 +0100

    More on pointers, array and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,11 +12,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	while (n > 0)
+	for (i = 0; n > 0; i++, n--)
 	{
 		s[i] = b;
-		i++;
-		n--;
 	}
+
 	return (s);
 }
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+nclude "main.h"
+
+/**
+ * *_memcpy - Entry
+ * @dest: memory area
+ * @src: source
+ * @n: length of src
+ * Description: copies memory area
+ * Return: pointer to dest.
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
