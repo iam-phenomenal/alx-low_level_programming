5ffa0b325424c656b5db9a0aa03bc56dcd17d06e
iam-phenomenal <ayandiranibrahim@outlook.com>
2022-10-11 01:45:04 +0100

    Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main- Entry
+ * Description: Print the file it was compiled from
+ * Return: 0(Success)
+*/
+int main(void)
+{
+	_putchar(__FILE__);
+	return (0);
+}
