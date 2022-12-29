8f92d3e6f542c095d974f310cc565d6666ec4694
Iam-phenomenal <ayandiranibrahim@outlook.com>
2022-12-29 16:13:35 +0000

    Additional tasks

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,63 +1,31 @@
 #include <stdio.h>
 
-
 /**
- * numLength - returns the lenth of string
- * @num : operand number
- * Return: number of digits
+ * main - Prints the first 52 fibonacci numbers
+ *
+ * Return: Nothing!
  */
-
-int numLength(int num)
-{
-	int length = 0;
-
-	if (!num)
-	{
-		return (1);
-	}
-
-	while (num)
-	{
-		num = num / 10;
-		length += 1;
-	}
-
-	return (length);
-}
-/**
- *  *main - prints the first 98 fibonaci sequences
- *  Return: 0
- */
-
 int main(void)
 {
-	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f1o = 0, f2o = 0, tmpo = 0;
-	short int i = 1, initial0s;
+	int i = 0;
+	long j = 1, k = 2;
 
-	while (i <= 98)
+	while (i < 50)
 	{
-		if (f1o > 0)
-			printf("%lu", f1o);
-		initial0s = numLength(mx) - 1 - numLength(f1);
-		while (f1o > 0 && initial0s > 0)
-		{
-			printf("%i", 0);
-			initial0s--;
-		}
-		printf("%lu", f1);
-
-		tmp = (f1 + f2) % mx;
-		tmpo = f1o + f2o + (f1 + f2) / mx;
-		f1 = f2;
-		f1o = f2o;
-		f2 = tmp;
-		f2o = tmpo;
-
-		if (i != 98)
-			printf(", ");
+		if (i == 0)
+			printf("%ld", j);
+		else if (i == 1)
+			printf(", %ld", k);
 		else
-			printf("\n");
-		i++;
+		{
+			k += j;
+			j = k - j;
+			printf(", %ld", k);
+		}
+
+		++i;
 	}
+
+	printf("\n");
 	return (0);
 }
